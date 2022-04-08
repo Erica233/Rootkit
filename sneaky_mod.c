20d3357cefdef64da394f9867020cbefb73a34f1
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-08 11:29:48 -0400

    skeleton

--- sneaky_mod.c
+++ sneaky_mod.c
@@ -16,20 +16,20 @@ static unsigned long *sys_call_table;
 
 // Helper functions, turn on and off the PTE address protection mode
 // for syscall_table pointer
-int enable_page_rw(void *ptr){
-  unsigned int level;
-  pte_t *pte = lookup_address((unsigned long) ptr, &level);
-  if(pte->pte &~_PAGE_RW){
-    pte->pte |=_PAGE_RW;
-  }
-  return 0;
+int enable_page_rw(void *ptr) {
+    unsigned int level;
+    pte_t *pte = lookup_address((unsigned long) ptr, &level);
+    if (pte->pte & ~_PAGE_RW) {
+        pte->pte |= _PAGE_RW;
+    }
+    return 0;
 }
 
-int disable_page_rw(void *ptr){
-  unsigned int level;
-  pte_t *pte = lookup_address((unsigned long) ptr, &level);
-  pte->pte = pte->pte &~_PAGE_RW;
-  return 0;
+int disable_page_rw(void *ptr) {
+    unsigned int level;
+    pte_t *pte = lookup_address((unsigned long) ptr, &level);
+    pte->pte = pte->pte & ~_PAGE_RW;
+    return 0;
 }
 
 // 1. Function pointer will be used to save address of the original 'openat' syscall.
@@ -38,9 +38,8 @@ int disable_page_rw(void *ptr){
 asmlinkage int (*original_openat)(struct pt_regs *);
 
 // Define your new sneaky version of the 'openat' syscall
-asmlinkage int sneaky_sys_openat(struct pt_regs *regs)
-{
-  // Implement the sneaky part here
+asmlinkage int sneaky_sys_openat(struct pt_regs *regs) {
+    // Implement the sneaky part here
 
 
 
@@ -48,57 +47,57 @@ asmlinkage int sneaky_sys_openat(struct pt_regs *regs)
 
 
 
-  return (*original_openat)(regs);
+    return (*original_openat)(regs);
 }
 
 // The code that gets executed when the module is loaded
-static int initialize_sneaky_module(void)
-{
-  // See /var/log/syslog or use `dmesg` for kernel print output
-  printk(KERN_INFO "Sneaky module being loaded.\n");
+static int initialize_sneaky_module(void) {
+    // See /var/log/syslog or use `dmesg` for kernel print output
+    printk(KERN_INFO
+    "Sneaky module being loaded.\n");
 
-  // Lookup the address for this symbol. Returns 0 if not found.
-  // This address will change after rebooting due to protection
-  sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
+    // Lookup the address for this symbol. Returns 0 if not found.
+    // This address will change after rebooting due to protection
+    sys_call_table = (unsigned long *) kallsyms_lookup_name("sys_call_table");
 
-  // This is the magic! Save away the original 'openat' system call
-  // function address. Then overwrite its address in the system call
-  // table with the function address of our new code.
-  original_openat = (void *)sys_call_table[__NR_openat];
-  
-  // Turn off write protection mode for sys_call_table
-  enable_page_rw((void *)sys_call_table);
-  
-  sys_call_table[__NR_openat] = (unsigned long)sneaky_sys_openat;
+    // This is the magic! Save away the original 'openat' system call
+    // function address. Then overwrite its address in the system call
+    // table with the function address of our new code.
+    original_openat = (void *) sys_call_table[__NR_openat];
 
-  // You need to replace other system calls you need to hack here
+    // Turn off write protection mode for sys_call_table
+    enable_page_rw((void *) sys_call_table);
 
+    sys_call_table[__NR_openat] = (unsigned long) sneaky_sys_openat;
 
+    // You need to replace other system calls you need to hack here
 
 
 
-  
-  // Turn write protection mode back on for sys_call_table
-  disable_page_rw((void *)sys_call_table);
-
-  return 0;       // to show a successful load 
-}  
 
 
-static void exit_sneaky_module(void) 
-{
-  printk(KERN_INFO "Sneaky module being unloaded.\n"); 
 
-  // Turn off write protection mode for sys_call_table
-  enable_page_rw((void *)sys_call_table);
+    // Turn write protection mode back on for sys_call_table
+    disable_page_rw((void *) sys_call_table);
 
-  // This is more magic! Restore the original 'open' system call
-  // function address. Will look like malicious code was never there!
-  sys_call_table[__NR_openat] = (unsigned long)original_openat;
+    return 0;       // to show a successful load
+}
 
-  // Turn write protection mode back on for sys_call_table
-  disable_page_rw((void *)sys_call_table);  
-}  
+
+static void exit_sneaky_module(void) {
+    printk(KERN_INFO
+    "Sneaky module being unloaded.\n");
+
+    // Turn off write protection mode for sys_call_table
+    enable_page_rw((void *) sys_call_table);
+
+    // This is more magic! Restore the original 'open' system call
+    // function address. Will look like malicious code was never there!
+    sys_call_table[__NR_openat] = (unsigned long) original_openat;
+
+    // Turn write protection mode back on for sys_call_table
+    disable_page_rw((void *) sys_call_table);
+}
 
 
 module_init(initialize_sneaky_module);  // what's called upon loading 
